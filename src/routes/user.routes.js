82f9d2fd908113814b4c7967d2620e6795a69e0a
Aakash vishwakarma <aakxsh17@gmail.com>
2024-09-21 01:58:15 +0530

    correct some errors

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import  {Router} from "express";
 import {upload} from "../middleware/multer.middleware.js" 
 import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 
 
