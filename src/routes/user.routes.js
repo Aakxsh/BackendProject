a3efa6ff6d60c1859ddeaab22f8e2ff9f63de7b6
Aakash vishwakarma <aakxsh17@gmail.com>
2024-09-28 00:16:59 +0530

    Add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,19 @@
 import  {Router} from "express";
 import {upload} from "../middleware/multer.middleware.js" 
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUsercoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory } from "../controllers/user.controller.js";
+    
 import { verifyJWT } from "../middleware/auth.middleware.js";
+
 
 
 
@@ -29,6 +41,28 @@ router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 export default router 
-router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/refresh-token").post
+(refreshAccessToken)
+
+router.route("/changed_Password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current_User").get(verifyJWT, getCurrentUser)
+
+router.route("/update_Account_Details").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar)
+
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), updateUsercoverImage)
+
+// when we use params
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/watch_History").get(verifyJWT, getWatchHistory)
+
+
+
+
+
 
 export {registerUser}
